9cbe8d694dbf218c0b134f12b183f7119473b1cf
ragl8391 <212429257+ragl8391@users.noreply.github.com>
2026-02-01 20:32:00 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/navigation.ts
@@ -0,0 +1,147 @@
+import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+import Week2 from 'src/pages/homes/week2.md';
+import week2research from 'src/pages/landing/week2research.md';
+
+export const headerData = {
+  links: [
+    {
+      text: 'Weekly Status Updates',
+      links: [
+        {
+          text: 'Week 2',
+          href: getPermalink('homes/personal'),
+        },
+        {
+          text: 'Week 3',
+          href: getPermalink('/homes/week3'),
+        },
+        {
+          text: 'Week 4',
+          href: getPermalink('/homes/week4'),
+        },
+        {
+          text: 'Week 5',
+          href: getPermalink('/homes/week5'),
+        },
+      ],
+    },
+    {
+      text: 'Research Updates',
+      links: [
+        {
+          text: 'GitHub and GitLab',
+          href: getPermalink('/landing/week2research'),
+        },
+        {
+          text: 'CI/CD Pipelines',
+          href: getPermalink('/landing/sales'),
+        },
+        {
+          text: 'Configuration Files',
+          href: getPermalink('/landing/click-through'),
+        },
+        {
+          text: 'Flake8',
+          href: getPermalink('/landing/product'),
+        },
+        {
+          text: 'cppcheck/cpplint',
+          href: getPermalink('/landing/pre-launch'),
+        },
+      ],
+    },
+    {
+      text: 'Blog',
+      links: [
+        {
+          text: 'Blog List',
+          href: getBlogPermalink(),
+        },
+        {
+          text: 'Article',
+          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
+        },
+        {
+          text: 'Article (with MDX)',
+          href: getPermalink('markdown-elements-demo-post', 'post'),
+        },
+        {
+          text: 'Category Page',
+          href: getPermalink('tutorials', 'category'),
+        },
+        {
+          text: 'Tag Page',
+          href: getPermalink('astro', 'tag'),
+        },
+      ],
+    },
+    {
+      text: 'Widgets',
+      href: '#',
+    },
+  ],
+  actions: [{ text: 'Download', href: 'https://github.com/arthelokyo/astrowind', target: '_blank' }],
+};
+
+export const footerData = {
+  links: [
+    {
+      title: 'Product',
+      links: [
+        { text: 'Features', href: '#' },
+        { text: 'Security', href: '#' },
+        { text: 'Team', href: '#' },
+        { text: 'Enterprise', href: '#' },
+        { text: 'Customer stories', href: '#' },
+        { text: 'Pricing', href: '#' },
+        { text: 'Resources', href: '#' },
+      ],
+    },
+    {
+      title: 'Platform',
+      links: [
+        { text: 'Developer API', href: '#' },
+        { text: 'Partners', href: '#' },
+        { text: 'Atom', href: '#' },
+        { text: 'Electron', href: '#' },
+        { text: 'AstroWind Desktop', href: '#' },
+      ],
+    },
+    {
+      title: 'Support',
+      links: [
+        { text: 'Docs', href: '#' },
+        { text: 'Community Forum', href: '#' },
+        { text: 'Professional Services', href: '#' },
+        { text: 'Skills', href: '#' },
+        { text: 'Status', href: '#' },
+      ],
+    },
+    {
+      title: 'Company',
+      links: [
+        { text: 'About', href: '#' },
+        { text: 'Blog', href: '#' },
+        { text: 'Careers', href: '#' },
+        { text: 'Press', href: '#' },
+        { text: 'Inclusion', href: '#' },
+        { text: 'Social Impact', href: '#' },
+        { text: 'Shop', href: '#' },
+      ],
+    },
+  ],
+  secondaryLinks: [
+    { text: 'Terms', href: getPermalink('/terms') },
+    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+  ],
+  socialLinks: [
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/arthelokyo/astrowind' },
+  ],
+  footNote: `
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/arthelokyo"> Arthelokyo</a> · All rights reserved.
+  `,
+};
